f06158497974a2915148a5a9b79d04b4a843399e
dovejb <dovejb@qq.com>
2022-03-15 16:39:18 +0800

    up

--- types.go
+++ types.go
@@ -8,6 +8,18 @@ type String string
 type Int int
 type Number float64
 
+func (s String) V() string {
+	return string(s)
+}
+
+func (i Int) V() int {
+	return int(i)
+}
+
+func (n Number) V() float64 {
+	return float64(n)
+}
+
 var (
 	StringType = reflect.TypeOf(String(""))
 	IntType    = reflect.TypeOf(Int(0))
